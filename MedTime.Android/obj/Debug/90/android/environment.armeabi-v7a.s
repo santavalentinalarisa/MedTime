f1f46bb624c7951de933d8b57eef3e0c42ca21f5
santavalentinalarisa <larisavalentina39@yahoo.com>
2020-05-13 22:51:28 +0300

    Adaugare proiect iOS

--- MedTime.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ MedTime.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"e08654dd-f130-423b-8222-c51be1f215ac"
+	.asciz	"1262c205-c5ee-465b-99bd-4ff0b4c74956"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
